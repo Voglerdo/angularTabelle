77f7a0c19bada6548bb8c501a418af4e14cd5f42
Dominik <120841465+Voglerdo@users.noreply.github.com>
2024-01-05 14:05:33 +0000

    changed columns

--- src/example/table-basic-example.ts
+++ src/example/table-basic-example.ts
@@ -2,23 +2,17 @@ import {Component} from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 
 export interface PeriodicElement {
+
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  ID: number;
+  age: number;
+  gender: string;
+  country: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {ID: 1, name: 'RR', age: 69, gender: 'Sigma',country: 'Österreich' },
+
 ];
 
 /**
@@ -32,11 +26,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
   imports: [MatTableModule],
 })
 export class TableBasicExample {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['ID', 'name', 'country','gender','age'];
   dataSource = ELEMENT_DATA;
 }
-
-
-/**  Copyright 2024 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at https://angular.io/license */
